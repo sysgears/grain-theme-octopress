58352d58feeab7b5a12b91d73dc2f61231216487
Anton Yarchuk <anton.yarchuk@sysgears.com>
2014-01-17 16:00:51 +0200

    Improved TagLib

---
+++ content/blog/2014-01-10-jsfiddle-tag.markdown
@@ -0,0 +1,21 @@
+---
+layout: post
+title: "jsFiddle tag"
+date: "2014-01-10 13:00"
+author: SysGears
+categories: [grain, groovy]
+comments: true
+published: true
+---
+
+<!--more-->
+
+####Code:
+
+```grain
+<%= jsfiddle shorttag: '8wcwq', width: '90%', skin: 'light', tabs: 'html,css,js,result' %>
+```
+
+####Result:
+
+<%= jsfiddle shorttag: '8wcwq', width: '90%', skin: 'light', tabs: 'html,css,js,result' %>
--- theme/src/com/sysgears/octopress/taglibs/OctopressTagLib.groovy
+++ theme/src/com/sysgears/octopress/taglibs/OctopressTagLib.groovy
@@ -17,16 +17,38 @@ class OctopressTagLib {
     /**
      * Renders a quote block which contains the quote text, author and source.
      *
-     * @attr content the quote content
-     * @attr author (optional) quote author
-     * @attr sourceTitle (optional) title of the quote source
-     * @athr sourceLink (optional) link to the quote source
+     * @attr content REQUIRED quote content
+     * @attr author the quote author
+     * @attr sourceTitle the title of the quote source
+     * @attr sourceLink the link to the quote source
      */
-    def blockquote = { Map model = null ->
+    def blockquote = { Map attrs ->
         // validates the tag attributes
-        if (!model.content) throw new IllegalArgumentException('Tag [blockquote] is missing required attribute [content]')
+        if (!attrs.content) throw new IllegalArgumentException('Tag [blockquote] is missing required attribute [content]')
 
-        taglib.include('/tags/blockquote.html', [quote: model])
+        taglib.include('/tags/blockquote.html', [quote: attrs])
+    }
+
+    /**
+     * Renders a jsFiddle iframe code.
+     *
+     * @attr shorttag REQUIRED jsFiddle shortcode
+     * @attr tabs jsFiddle tabs to show, 'js,resources,html,css,result' by default
+     * @attr skin jsFiddle skin name, 'light' by default
+     * @attr width iframe width, 100% by default
+     * @attr height iframe height, '300px' by default
+     */
+    def jsfiddle = { Map attrs ->
+        // validates the tag attributes
+        if (!attrs.shorttag) throw new IllegalArgumentException('Tag [jsfiddle] is missing required attribute [shorttag]')
+
+        // sets default values
+        attrs.tabs = attrs.tabs ?: 'js,resources,html,css,result'
+        attrs.skin = attrs.skin ?: 'light'
+        attrs.width = attrs.width ?: '100%'
+        attrs.height = attrs.height ?: '300px'
+
+        taglib.include('/tags/jsfiddle.html', [jsfiddle: attrs])
     }
 
     /**
@@ -34,15 +56,15 @@ class OctopressTagLib {
      * <br />
      * The quote must be surrounded by '{/' '/}' tags, for instance: 'Lorem ipsum {/dolor/} sit amet'.
      *
-     * @attr content content that contains a quote
-     * @attr align (optional) quote position, can be either 'right' or 'left', 'right' is by default
+     * @attr content REQUIRED content that contains a quote
+     * @attr align quote position, can be either 'right' or 'left', 'right' is by default
      */
-    def pullquote = { model ->
+    def pullquote = { Map attrs ->
         // validates the tag attributes
-        if (!model.content) throw new IllegalArgumentException('Tag [pullquote] is missing required attribute [content]')
+        if (!attrs.content) throw new IllegalArgumentException('Tag [pullquote] is missing required attribute [content]')
 
-        String content = model.content
-        String align = model.align ?: 'right'
+        String content = attrs.content
+        String align = attrs.align ?: 'right'
 
         // finds quote which is surrounded by '{/' '/}' tags
         String quote = content.find(/\{\/(.*)\/\}/) { match, quote -> quote }
@@ -55,43 +77,54 @@ class OctopressTagLib {
     /**
      * Embeds a gist into the page.
      *
-     * @attr id unique gist identifier
+     * @attr id REQUIRED unique gist identifier
      */
-    def gist = { Map model = null ->
+    def gist = { Map attrs ->
         // validates the tag attributes
-        if (!model.id) throw new IllegalArgumentException('Tag [gist] is missing required attribute [id]')
+        if (!attrs.id) throw new IllegalArgumentException('Tag [gist] is missing required attribute [id]')
 
-        taglib.include('/tags/gist.html', [gist: model])
+        taglib.include('/tags/gist.html', [gist: attrs])
     }
 
     /**
-     * Generates html tag for an image
+     * Generates html tag for an image.
      *
-     * @attr location image location
-     * @attr width (optional) image width
-     * @attr height (optional) image height
+     * @attr src REQUIRED image location
+     * @attr height image height
+     * @attr width image width
+     * @attr alt alternate text for the image
      */
-    def img = { Map model ->
-        if (!model.location) throw new IllegalArgumentException('Tag [img] is missing required attribute [location]')
+    def img = { Map attrs ->
+        // validates the tag attributes
+        if (!attrs.src) throw new IllegalArgumentException('Tag [img] is missing required attribute [src]')
 
-        def widthStr = model.width ? " width=\"${model.width}\"" : ""
-        def heightStr = model.height ? " height=\"${model.height}\"" : ""
+        attrs.src = attrs.src.startsWith('/') ? taglib.r(attrs.src) : attrs.src
+        taglib.include('/tags/img.html', [img: attrs])
+    }
 
-        "<img${widthStr}${heightStr} src=\"${taglib.r(model.location)}\" alt=\"image\">"
+    /**
+     * Prevents groovy code parsing.
+     *
+     * @attr text REQUIRED groovy code which should not be be parsed.
+     */
+    def raw = { Map attrs ->
+        if (!attrs.text) throw new IllegalArgumentException('Tag [raw] is missing required attribute [text]')
+
+        attrs.text
     }
 
     /**
      * Embeds a video into the page.
      *
-     * @attr urls links to the videos
-     * @attr poster (optional) link to a poster
-     * @attr wight (optional) video wight
-     * @attr height (optional) video height
+     * @attr urls REQUIRED links to the videos
+     * @attr poster link to a poster
+     * @attr wight video wight
+     * @attr height video height
      */
-    def video = { Map model = null ->
+    def video = { Map attrs ->
 
         // validates the tag attributes
-        if (!model.urls || !(model.urls instanceof List) || model.urls.isEmpty()) {
+        if (!attrs.urls || !(attrs.urls instanceof List) || attrs.urls.isEmpty()) {
             throw new IllegalArgumentException('Tag [video] is missing required attribute [urls]')
         }
 
@@ -101,7 +134,7 @@ class OctopressTagLib {
 
         def videoSources = []
 
-        model.urls.each {
+        attrs.urls.each {
             def type = types."${it.find(/[^\.]+$/)}"
             if (type) {
                 videoSources << [url: it, type: type]
@@ -112,23 +145,23 @@ class OctopressTagLib {
             throw new IllegalArgumentException("Tag [video] does not support file formats of any of the provided video sources")
         }
 
-        model << [sources: videoSources]
+        attrs << [sources: videoSources]
 
-        taglib.include('/tags/video.html', [video: model])
+        taglib.include('/tags/video.html', [video: attrs])
     }
 
     /**
      * Converts title by applying Title Case capitalizing convention (capitalizes all principal words).
      *
-     * @attr title the title to convert
+     * @attr title REQUIRED the title to convert
      */
-    def titleCase = { Map model ->
-        if (!model.title) throw new IllegalArgumentException('Tag [titleCase] is missing required attribute [title]')
+    def titleCase = { Map attrs ->
+        if (!attrs.title) throw new IllegalArgumentException('Tag [titleCase] is missing required attribute [title]')
 
         def nonPrincipalWords = ['a', 'an', 'and', 'as', 'at', 'but', 'by', 'en', 'for', 'if', 'in',
                 'of', 'on', 'or', 'the', 'to', 'v', 'v.', 'via', 'vs', 'vs.']
 
-        model.title.split(' ').inject(new StringBuilder()) {result, word ->
+        attrs.title.split(' ').inject(new StringBuilder()) {result, word ->
             word in nonPrincipalWords ? result.append(word) : result.append(StringUtils.capitalize(word))
             result.append(' ')
         } .toString().trim()
@@ -137,13 +170,13 @@ class OctopressTagLib {
     /**
      * Converts a date to XML date time format: 2013-12-31T12:49:00+07:00
      *
-     * @attr date the date to convert
+     * @attr date REQUIRED the date to convert
      */
-    def xmlDateTime = { Map model ->
-        if (!model.date) throw new IllegalArgumentException('Tag [xmlDateTime] is missing required attribute [date]')
+    def xmlDateTime = { Map attrs ->
+        if (!attrs.date) throw new IllegalArgumentException('Tag [xmlDateTime] is missing required attribute [date]')
 
-        def tz = String.format('%tz', model.date)
+        def tz = String.format('%tz', attrs.date)
 
-        String.format("%tFT%<tT${tz.substring(0, 3)}:${tz.substring(3)}", model.date)
+        String.format("%tFT%<tT${tz.substring(0, 3)}:${tz.substring(3)}", attrs.date)
     }
 }
